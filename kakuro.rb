5c50d91da0656d7e44d91d35c530c425f0c032d5
James Petry <gavilancomun@gmail.com>
2016-07-15 14:02:41 +0100

    test

--- kakuro.rb
+++ kakuro.rb
@@ -20,14 +20,63 @@ class DownAcrossCell
     if (obj == nil)
       return false
     end
-    if (undefined == obj.down)
-      return false
-    end
     return (@down == obj.down) && (@across == obj.across)
   end
 
   def draw
-    "   " + pad2(@down) + "\\" + pad2(@across) + "  "
+    "   " + sprintf("%02d", @down) + "\\" + sprintf("%02d", @across) + "  "
+  end
+end
+
+class AcrossCell 
+  attr_accessor :across
+
+  def initialize(across)
+    @across = across
+  end
+
+  def to_s
+    "AcrossCell[" + @across + "]"
+  end
+
+  def  equals(obj)
+    if (this == obj)
+      return true
+    end
+    if (obj == nil)
+      return false
+    end
+    return (@across == obj.across)
+  end
+
+  def draw
+    "   --\\" + sprintf("%02d", @across) + "  "
+  end
+end
+
+class DownCell 
+  attr_accessor :down
+
+  def initialize(down)
+    @down = down
+  end
+
+  def to_s
+    "DownCell[" + @down + "]"
+  end
+
+  def  equals(obj)
+    if (this == obj)
+      return true
+    end
+    if (obj == nil)
+      return false
+    end
+    return (@down == obj.down)
+  end
+
+  def draw
+    "   " + sprintf("%02d", @down) + "\\--  "
   end
 end
 
@@ -40,25 +89,45 @@ class ValueCell
 
   def draw
     if (1 == @values.length)
-      "     " + @values[0] + "    "
-    else {
-      " " + (1..9)
-        .map {|n| @values.includes?(n) ? "" + n : "."}
-        .join("")
+      "     " + @values[0].to_s + "    "
+    else
+      " " + (1..9).map {|n| (@values.include?(n) ? n.to_s : ".")}.join
     end
   end
 
+end
+
+class EmptyCell
+  def draw
+    "   -----  "
+  end
 end
 
 def da(down, across)
   DownAcrossCell.new(down, across)
 end
 
+def d(down)
+  DownCell.new(down)
+end
+
+def a(across)
+  AcrossCell.new(across)
+end
+
+def e
+  EmptyCell.new
+end
+
 def v (*arguments)
   if (0 == arguments.length)
-    return new ValueCell.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    return ValueCell.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
   else
-    return new ValueCell.new(arguments)
+    return ValueCell.new(arguments)
   end
+end
+
+def drawRow(row)
+  row.map {|v| v.draw}.join + "\n";
 end
 
